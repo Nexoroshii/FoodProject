2c8138c6c302c852aa75b001fda70c000e1acc13
Egor <e.moisei1@gmail.com>
2023-01-09 23:19:47 +0300

    add timer on ModalWindow and check for end of the page

--- js/script.js
+++ js/script.js
@@ -103,13 +103,17 @@ window.addEventListener("DOMContentLoaded", () => {
     modal = document.querySelector(".modal"),
     modalCloseBtn = document.querySelector("[data-close]");
 
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    //modal.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+
+  clearInterval(modalTimerId);
+  }
+
   modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      modal.classList.add("show");
-      modal.classList.remove("hide");
-      //modal.classList.toggle("show");
-      document.body.style.overflow = "hidden";
-    });
+    btn.addEventListener("click", openModal);
   });
 
   // modalTrigger.addEventListener("click", () => {
@@ -118,6 +122,7 @@ window.addEventListener("DOMContentLoaded", () => {
   //   modal.classList.toggle('show');
   //   document.body.style.overflow = 'hidden';
   // });
+
   function closeModal() {
     modal.classList.add("hide");
     modal.classList.remove("show");
@@ -132,9 +137,21 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  document.addEventListener('keydown', (e) =>{
-    if(e.code === 'Escape' && modal.classList.contains('show')){
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
       closeModal();
     }
   });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll(){
+    if(window.pageYOffset + document.documentElement.clientHeight>=document.documentElement.scrollHeight){
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  
+  window.addEventListener('scroll', showModalByScroll);
 });
